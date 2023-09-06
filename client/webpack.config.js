709c4aff91bb97dd644c137eef1452cb7feaf012
Halvess <halvess@live.com>
2023-09-06 01:08:33 -0300

    adjustments for deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,5 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-const dotenv = require('dotenv')
-
-const env = dotenv.config().parsed
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next])
-  return prev
-}, {})
-
 module.exports = {
 
     entry: './src/index.js',
@@ -22,7 +13,6 @@ module.exports = {
         new HTMLWebpackPlugin({
             template: './src/index.html'
         }),
-        new webpack.DefinePlugin(envKeys)
     ],
 
     devServer:{
